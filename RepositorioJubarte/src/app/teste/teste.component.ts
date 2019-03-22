a6e31fe01fe23e1f7d90525825899664679061e0
jonathan <jonathanxavier556@gmail.com>
2019-03-22 12:47:55 -0300

    commit de rotas

---
+++ RepositorioJubarte/src/app/teste/teste.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teste',
+  templateUrl: './teste.component.html',
+  styleUrls: ['./teste.component.css']
+})
+export class TesteComponent {
+
+
+}
