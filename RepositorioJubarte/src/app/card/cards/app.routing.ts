bd25313cb43a1d669ca5174ab126f053e4579162
jonathan <jonathanxavier556@gmail.com>
2019-04-05 12:46:02 -0300

    jonathan

--- RepositorioJubarte/src/app/card/cards/app.routing.ts
+++ RepositorioJubarte/src/app/card/cards/app.routing.ts
@@ -1,9 +1,11 @@
 import { ModuleWithProviders } from '@angular/core';
 import{Routes, RouterModule} from '@angular/router';
 import { TesteComponent } from 'src/app/teste/teste.component';
+import { HomeComponent } from 'src/app/home/home.component';
 
 
 const APP_ROUTES: Routes = [
-  {path:'teste',component:TesteComponent},
+  {path:'',component:HomeComponent},
+  {path:'teste',component:TesteComponent}
 ];
 export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
