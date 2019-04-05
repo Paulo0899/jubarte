6eb5f4218e3f649f374a1e10342afddb80bcb1fb
Paulo Ricardo <Paulo0899@gmail.com>
2019-04-05 13:28:42 -0300

    altered carbon

--- RepositorioJubarte/src/app/app.component.ts
+++ RepositorioJubarte/src/app/app.component.ts
@@ -5,29 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-<<<<<<< HEAD
 
-=======
+export class AppComponent {
   card = [
     {
-<<<<<<< HEAD
       url:'https://i.pinimg.com/564x/28/96/aa/2896aa637c462f0c2483c1412486b5d0.jpg?b=t',
       titulo:'Dracula'
     },
     {
       url:'https://i.pinimg.com/564x/0d/d2/77/0dd277d320b11d37d3d3ad42a9cd69a4.jpg?b=t://vignette.wikia.nocookie.net/naruto/images/2/27/Kakashi_Hatake.png/revision/latest?cb=20170628120149',
       titulo: 'fabrica de chocolates'
-=======
-      url: 'https://i.pinimg.com/564x/28/96/aa/2896aa637c462f0c2483c1412486b5d0.jpg?b=t',
-      descricao: 'Dracula'
-    },
-    {
-      url:
-// tslint:disable-next-line: max-line-length
-      'https://i.pinimg.com/564x/0d/d2/77/0dd277d320b11d37d3d3ad42a9cd69a4.jpg?b=t://vignette.wikia.nocookie.net/naruto/images/2/27/Kakashi_Hatake.png/revision/latest?cb=20170628120149',
-      descricao: 'fabrica de chocolates'
->>>>>>> 52ccebd521d3a70179ab05e8f22220a8a2a8ae6d
 
     },
     {
@@ -35,15 +22,8 @@ export class AppComponent {
       titulo: 'whatchman'
     },
     {
-<<<<<<< HEAD
       url:'https://i.pinimg.com/564x/99/c2/80/99c2801386f788ba72c13ec949ec55c7.jpg?b=t://i.pinimg.com/564x/be/b4/6e/beb46e838468cdd807e9a1fa57b39547.jpg',
       titulo: 'whatchman'
-=======
-      url:
-// tslint:disable-next-line: max-line-length
-      'https://i.pinimg.com/564x/99/c2/80/99c2801386f788ba72c13ec949ec55c7.jpg?b=t://i.pinimg.com/564x/be/b4/6e/beb46e838468cdd807e9a1fa57b39547.jpg',
-      descricao: 'whatchman'
->>>>>>> 52ccebd521d3a70179ab05e8f22220a8a2a8ae6d
     },
     {
       url:'https://i.pinimg.com/564x/7e/f2/40/7ef240ff6c17de6192bb6399e3289a15.jpg',
@@ -58,5 +38,4 @@ export class AppComponent {
       titulo: 'whatchman'
     }
   ];
->>>>>>> 52ccebd521d3a70179ab05e8f22220a8a2a8ae6d
 }
